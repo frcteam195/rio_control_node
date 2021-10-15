26c4308663c2a8a3ca77b87b1330fcfc60df5f0b
Michael Todd <eltodd@gmail.com>
2021-10-15 18:33:08 -0400

    Initial robot status support

--- src/listener.cpp
+++ src/listener.cpp
@@ -12,6 +12,7 @@
 #include "JoystickStatus.pb.h"
 
 #include <rio_control_node/Joystick_Status.h>
+#include <rio_control_node/Robot_Status.h>
 
 ros::NodeHandle * node;
 
@@ -26,6 +27,68 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg)
 constexpr unsigned int str2int(const char* str, int h = 0)
 {
     return !str[h] ? 5381 : (str2int(str, h+1) * 33) ^ str[h];
+}
+
+void process_joystick_status(zmq_msg_t &message)
+{
+  static ros::Publisher joystick_pub = node->advertise<rio_control_node::Joystick_Status>("JoystickStatus", 1);
+  static ck::JoystickStatus status;
+
+  void * data = zmq_msg_data(&message);
+  bool parse_result = status.ParseFromArray(data, zmq_msg_size(&message));
+  if (parse_result)
+  {
+
+    rio_control_node::Joystick_Status joystick_status;
+
+    for (int i = 0; i < status.joysticks_size(); i++)
+    {
+      const ck::JoystickStatus::Joystick& joystick = status.joysticks(i);
+      rio_control_node::Joystick stick;
+
+      stick.index = joystick.index();
+      
+      uint32_t buttons = joystick.buttons();
+      for (int j = 0; j < 32; j++)
+      {
+        stick.buttons.push_back((buttons >> j) & 0x0001);
+      }
+
+      for(int j = 0; j < joystick.axes_size(); j++)
+      {
+        stick.axes.push_back(joystick.axes(j));
+      }
+
+      for(int j = 0; j < joystick.povs_size(); j++)
+      {
+        stick.povs.push_back(joystick.povs(j));
+      }
+
+      joystick_status.joysticks.push_back(stick);
+
+    }
+    joystick_pub.publish(joystick_status);
+  }
+}
+
+void process_robot_status(zmq_msg_t &message)
+{    
+  static ck::RobotStatus status;
+  static ros::Publisher robot_status_pub = node->advertise<rio_control_node::Robot_Status>("RobotStatus", 1);
+
+  void * data = zmq_msg_data(&message);
+  bool parse_result = status.ParseFromArray(data, zmq_msg_size(&message));
+  
+  if (parse_result)
+  {
+    rio_control_node::Robot_Status robot_status;
+    robot_status.alliance = status.alliance();
+    robot_status.robot_state = status.robot_state();
+    robot_status.match_time = status.match_time();
+    robot_status.game_data = status.game_data().c_str();  
+    ROS_INFO("Got %d bytes : String %s : C_String %s : Hex %X", status.game_data().size(), status.game_data(), status.game_data().c_str(), status.game_data().c_str());
+    robot_status_pub.publish(robot_status);
+  }
 }
 
 void robot_receive_loop ()
@@ -44,7 +107,6 @@ void robot_receive_loop ()
 
   ROS_INFO("WOOGITY WOOGITY test WOO %d %d %d", context, subscriber, rc);
 
-  ros::Publisher joystick_pub = node->advertise<rio_control_node::Joystick_Status>("JoystickStatus", 1);
 
   while (ros::ok())
   {
@@ -58,47 +120,12 @@ void robot_receive_loop ()
     {
       case str2int("joystickstatus"):
         {
-          static ck::JoystickStatus status;
-          void * data = zmq_msg_data(&message);
-          bool parse_result = status.ParseFromArray(data, zmq_msg_size(&message));
-          if (parse_result)
-          {
-
-            rio_control_node::Joystick_Status joystick_status;
-
-            for (int i = 0; i < status.joysticks_size(); i++)
-            {
-              const ck::JoystickStatus::Joystick& joystick = status.joysticks(i);
-              rio_control_node::Joystick stick;
-
-              stick.index = joystick.index();
-              
-              uint32_t buttons = joystick.buttons();
-              for (int j = 0; j < 32; j++)
-              {
-                stick.buttons.push_back((buttons >> j) & 0x0001);
-              }
-
-              for(int j = 0; j < joystick.axes_size(); j++)
-              {
-                stick.axes.push_back(joystick.axes(j));
-              }
-
-              for(int j = 0; j < joystick.povs_size(); j++)
-              {
-                stick.povs.push_back(joystick.povs(j));
-              }
-
-              joystick_status.joysticks.push_back(stick);
-
-            }
-            joystick_pub.publish(joystick_status);
-          }
+          process_joystick_status(message);
         }
         break; 
       case str2int("robotstatus"):
         {
-          static ck::RobotStatus status;
+          process_robot_status(message);
         }
         break;
       default:
@@ -106,14 +133,7 @@ void robot_receive_loop ()
         break;
     }
 
-    
-    // bool parse_result = status.ParseFromArray(data, zmq_msg_size(&message));
-    
-    // ROS_INFO("Parse result: %d", parse_result);
-    // ROS_INFO("State: %s", ck::RobotStatus_RobotState_Name(status.robot_state()).c_str());
-    // ROS_INFO("Alliance: %s", ck::RobotStatus_Alliance_Name(status.alliance()).c_str());
-    // ROS_INFO("Time: %f", status.match_time());
-    // ROS_INFO("Game Data: %s", status.game_data().c_str());
+
 
     zmq_msg_close(&message);
   }
